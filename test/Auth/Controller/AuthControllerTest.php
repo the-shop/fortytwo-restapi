93798617635bc0698769ffc8ce0bc73c75fe1071
ivankristic <thechosen.os@gmail.com>
2017-12-14 17:20:57 +0100

    Auth functional tests - functional tests for `/resetPassword` and `/forgotPassword` - new `DummySendGrid` test class - updated `dummy_module.php` config - added registering services in `DummyModule`

--- test/Auth/Controller/AuthControllerTest.php
+++ test/Auth/Controller/AuthControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Framework\RestApi\Test\Auth\Controller;
 
+use Framework\Base\Application\Exception\NotFoundException;
 use Framework\Base\Test\Dummies\TestModel;
 use Framework\Base\Test\Dummies\TestRepository;
 use Framework\Http\Response\HttpResponseInterface;
@@ -21,7 +22,10 @@ class AuthControllerTest extends UnitTest
     {
         parent::setUp();
 
-        $resourceConfig = ['tests' => TestRepository::class];
+        $resourceConfig = [
+            'tests' => TestRepository::class,
+            'users' => TestRepository::class
+        ];
 
         $this->getApplication()
              ->getRepositoryManager()
@@ -29,19 +33,29 @@ class AuthControllerTest extends UnitTest
 
         $repository = $this->getApplication()
                            ->getRepositoryManager()
-                           ->getRepositoryFromResourceName('tests');
+                           ->getRepositoryFromResourceName('users');
 
         $model = new TestModel();
 
         $model->defineModelAttributes(
             $this->getApplication()
                  ->getConfiguration()
-                 ->getPathValue('models.Test.fields')
+                 ->getPathValue('models.User.fields')
         )
               ->setApplication($this->getApplication())
               ->setAttribute('email', 'test@test.com')
               ->setAttribute('password', 'test123')
-              ->setAttribute('role', 'standard');
+              ->setAttribute('role', 'standard')
+              ->setPrimaryKey($repository->getModelPrimaryKey())
+              ->setCollection($repository->getCollection())
+              ->setApplication($this->getApplication())
+              ->setRepository($repository)
+              ->setDatabaseAddress($this->getApplication()
+                                        ->getConfiguration()
+                                        ->getPathValue('env.DATABASE_ADDRESS'))
+              ->setDatabase($this->getApplication()
+                                 ->getConfiguration()
+                                 ->getPathValue('env.DATABASE_NAME'));
 
         $repository->getPrimaryAdapter()
                    ->setLoadOneResult($model);
@@ -95,7 +109,7 @@ class AuthControllerTest extends UnitTest
         $this::assertEquals('Auth strategy not implemented', $response->getBody()['errors'][0]);
         $this::assertEquals(500, $response->getCode());
 
-        $authModel['tests'] = ['strategy' => 'Imagined', 'credentials' => ['email', 'password', 'oneTooMany']];
+        $authModel['users'] = ['strategy' => 'Imagined', 'credentials' => ['email', 'password', 'oneTooMany']];
 
         $this->getApplication()
              ->getRepositoryManager()
@@ -104,6 +118,194 @@ class AuthControllerTest extends UnitTest
         $response = $this->makeHttpRequest('POST', '/login', $post);
 
         $this::assertEquals('No strategy registered with that name', $response->getBody()['errors'][0]);
+        $this::assertEquals(400, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testForgotPassword()
+    {
+        $post = [
+            'email' => 'test@test.com'
+        ];
+
+        $response = $this->makeHttpRequest('POST', '/forgotpassword', $post);
+
+        $this::assertEquals(
+            'You will shortly receive an email with the link to reset your password.',
+            $response->getBody()
+        );
+        $this::assertEquals(200, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testForgotPasswordNoEmail()
+    {
+        $post = [];
+
+        $response = $this->makeHttpRequest('POST', '/forgotpassword', $post);
+
+        $this::assertEquals('Email field missing.', $response->getBody()['errors'][0]);
+        $this::assertEquals(400, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testForgotPasswordUserNotFound()
+    {
+        $post = [
+            'email' => 'testic@test.com'
+        ];
+
+        $this->getApplication()
+             ->getRepositoryManager()
+             ->getPrimaryAdapter('users')
+             ->setLoadOneResult(null);
+
+        $response = $this->makeHttpRequest('POST', '/forgotpassword', $post);
+
+        $this::assertEquals('User not found.', $response->getBody()['errors'][0]);
+        $this::assertEquals(404, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testResetPassword()
+    {
+        $post = [
+            'email' => 'test@test.com'
+        ];
+
+        $this->makeHttpRequest('POST', '/forgotpassword', $post);
+
+        $token = $this->getApplication()
+                      ->getRepositoryManager()
+                      ->getRepositoryFromResourceName('users')
+                      ->loadOneBy($post)
+                      ->getAttribute('passwordResetToken');
+
+        $post = [
+            'token' => $token,
+            'newPassword' => 'test12',
+            'repeatNewPassword' => 'test12'
+        ];
+
+        $response = $this->makeHttpRequest('POST', '/resetpassword', $post);
+
+        $this::assertEquals('Password successfully changed.', $response->getBody());
+        $this::assertEquals(200, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testResetPasswordTokenMissing()
+    {
+        $post = [];
+
+        $response = $this->makeHttpRequest('POST', '/resetpassword', $post);
+
+        $this::assertEquals('Token not provided.', $response->getBody()['errors'][0]);
+        $this::assertEquals(404, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testResetPasswordUserNotFound()
+    {
+        $post = [
+            'email' => 'test@test.com'
+        ];
+
+        $this->makeHttpRequest('POST', '/forgotpassword', $post);
+
+        $post = [
+            'token' => '123',
+            'newPassword' => 'test12',
+            'repeatNewPassword' => 'test12'
+        ];
+
+        $this->getApplication()
+             ->getRepositoryManager()
+             ->getPrimaryAdapter('users')
+             ->setLoadOneResult(null);
+
+        $response = $this->makeHttpRequest('POST', '/resetpassword', $post);
+
+        $this::assertEquals('Invalid token provided.', $response->getBody()['errors'][0]);
+        $this::assertEquals(404, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testResetPasswordTokenExpired()
+    {
+        $post = [
+            'email' => 'test@test.com'
+        ];
+
+        $this->makeHttpRequest('POST', '/forgotpassword', $post);
+
+        $model = $this->getApplication()
+                      ->getRepositoryManager()
+                      ->getRepositoryFromResourceName('users')
+                      ->loadOneBy($post);
+
+        $model->setAttribute(
+            'passwordResetTime',
+            $model->getAttribute('passwordResetTime') - (30*60*60)
+        );
+
+        $token = $model->getAttribute('passwordResetToken');
+
+        $post = [
+            'token' => $token
+        ];
+
+        $response = $this->makeHttpRequest('POST', '/resetpassword', $post);
+
+        $this::assertEquals(
+            'Token has expired.',
+            $response->getBody()['errors'][0]
+        );
         $this::assertEquals(500, $response->getCode());
+    }
+
+    /**
+     *
+     */
+    public function testResetPasswordMissingNewPassword()
+    {
+        $post = [
+            'email' => 'test@test.com'
+        ];
+
+        $this->makeHttpRequest('POST', '/forgotpassword', $post);
+
+        $token = $this->getApplication()
+                      ->getRepositoryManager()
+                      ->getRepositoryFromResourceName('users')
+                      ->loadOneBy($post)
+                      ->getAttribute('passwordResetToken');
+
+        $post = [
+            'token' => $token,
+            'repeatNewPassword' => 'test12'
+        ];
+
+        $response = $this->makeHttpRequest('POST', '/resetpassword', $post);
+
+        $this::assertEquals(
+            'newPassword and repeatNewPassword fields must be provided and must not be empty!',
+            $response->getBody()['errors'][0]
+        );
+        $this::assertEquals(400, $response->getCode());
     }
 }
